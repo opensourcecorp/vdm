cb35314903db48f040b8512a84af55fbc6e77286
Ryan J. Price <ryapric@gmail.com>
2024-07-28 21:08:05 -0500

    GHA failed and I'm not sure why

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -20,15 +20,17 @@ printf 'Current version listed in VERSION file: %s\n' "${current_listed_version:
 failures=()
 
 if [[ "${current_git_branch}" == 'main' ]] ; then
+  printf 'On main branch, will manage tag checks & creation\n'
+
   # Fail if we forgot to bump VERSION
-  if [[ "${latest_git_tag}" == "${current_listed_version}" ]] ; then
+  if [[ "${latest_git_tag}" == "v${current_listed_version}" ]] ; then
     printf 'ERROR: Identifier in VERSION still matches what is tagged on the main branch -- did you forget to update?\n' > /dev/stderr
     failures+=('forgot-to-bump-VERSION')
   fi
 
   # Fail if we forgot to bump versions across files
   old_git_status="$(git status | grep -i -E 'modified')"
-  make -s bump-versions old_version="${current_listed_version}"
+  make -s bump-versions old_version="${current_listed_version}" > /dev/null
   new_git_status="$(git status | grep -i -E 'modified')"
   if [[ "$(diff <(echo "${old_git_status}") <(echo "${new_git_status}") | wc -l)" -gt 0 ]] ; then
     printf 'ERROR: Files modified by version-bump check -- did you forget to update versions across the repo to match VERSION?\n' > /dev/stderr
